265c41e111ae1ee962349b2a08d77fc50e49a096
YuriVini <yuri.ybgt.yfo@gmail.com>
2021-05-23 10:25:15 -0300

    Removing a line

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import PopUpNavigation from './PopUpNavigation';
 
-//LEMBRAR DE TIRAR PORRA
-import Payment from './screens/Payment';
-
-
 export default function App() {
   return (
     <>
